1088032c97662c8bf3a78018ccb11ea739a35867
GiomarMC <gmunozcu@unsa.edu.pe>
2026-02-16 15:16:00 -0500

    feat(users): add filtering by usuario, tienda and rol in UsuarioTiendaViewSet (#12)

--- core/apps/users/views/rol_tienda.py
+++ core/apps/users/views/rol_tienda.py
@@ -2,9 +2,21 @@ from rest_framework import viewsets
 from apps.users.models import UsuarioTienda
 from apps.users.serializers.rol_tienda import AsignarRolTiendaSerializer
 from apps.users.permissions import IsSuperUser
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 
 class UsuarioTiendaViewSet(viewsets.ModelViewSet):
     queryset = UsuarioTienda.objects.all()
     serializer_class = AsignarRolTiendaSerializer
-    permission_classes = [IsSuperUser]
+    permission_classes = [IsSuperUser, IsAuthenticated]
+
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['tienda', 'rol', 'usuario']
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        instance.usuario.is_active = False
+        instance.usuario.save()
+        return Response({"message": "Usuario desactivado"})
--- core/config/settings/base.py
+++ core/config/settings/base.py
@@ -61,6 +61,7 @@ INSTALLED_APPS += [
     "rest_framework.authtoken",
     "corsheaders",
     "drf_spectacular",
+    "django_filters",
 ]
 
 MIDDLEWARE = [
@@ -156,6 +157,9 @@ REST_FRAMEWORK = {
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ],
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "DEFAULT_FILTER_BACKENDS": [
+        "django_filters.rest_framework.DjangoFilterBackend",
+    ],
 }
 
 SIMPLE_JWT = {
