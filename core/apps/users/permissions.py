42406b8ebf051d834aa7b1f1ce93225abb7228cb
GiomarMC <gmunozcu@unsa.edu.pe>
2026-02-16 16:59:31 -0500

    feat(users): implement custom DRF permissions by role and store (#13)

--- core/apps/users/permissions.py
+++ core/apps/users/permissions.py
@@ -1,6 +1,53 @@
 from rest_framework.permissions import BasePermission
+from apps.users.constants import Roles
 
 
-class IsSuperUser(BasePermission):
+class IsAuthenticatedAndHasRole(BasePermission):
+    allow_roles = []
+
     def has_permission(self, request, view):
-        return request.user and request.user.is_superuser
+        user = request.user
+
+        if not user or not user.is_authenticated:
+            return False
+
+        if user.is_superuser:
+            return True
+
+        return user.tiendas.filter(
+            rol__nombre__in=self.allow_roles
+        ).exists()
+
+
+class IsOwner(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.is_superuser
+
+
+class IsAdmin(IsAuthenticatedAndHasRole):
+    allow_roles = [Roles.ADMIN]
+
+
+class IsWorker(IsAuthenticatedAndHasRole):
+    allow_roles = [Roles.TRABAJADOR]
+
+
+class CanViewEarnings(IsAuthenticatedAndHasRole):
+    allow_roles = [Roles.ADMIN]
+
+
+class IsAdminOfStore(BasePermission):
+
+    def has_object_permission(self, request, view, obj):
+        user = request.user
+
+        if not user.is_authenticated:
+            return False
+
+        if user.is_superuser:
+            return True
+
+        return user.tiendas.filter(
+            tienda=obj.tienda,
+            rol__nombre=Roles.ADMIN
+        ).exists()
