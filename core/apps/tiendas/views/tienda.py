1a6f4a831393ff1b1c033f9f4fed7aeea8ad47a9
GiomarMC <gmunozcu@unsa.edu.pe>
2026-02-19 10:19:21 -0500

    refactor(tiendas): update permission rules in Tienda views

--- core/apps/tiendas/views/tienda.py
+++ core/apps/tiendas/views/tienda.py
@@ -1,12 +1,12 @@
 from rest_framework import viewsets
-from apps.users.permissions import IsOwner
+from apps.users.permissions import IsOwnerOnly
 from apps.tiendas.models import Tienda
 from apps.tiendas.serializers.tienda import TiendaSerializer
 
 
 class TiendaViewSet(viewsets.ModelViewSet):
     serializer_class = TiendaSerializer
-    permission_classes = [IsOwner]
+    permission_classes = [IsOwnerOnly]
 
     def get_queryset(self):
         return Tienda.objects.filter(id_duenio=self.request.user)
