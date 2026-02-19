39574d59ae1162cf453be91f8a0f0347d6ab2998
GiomarMC <gmunozcu@unsa.edu.pe>
2026-02-19 10:00:25 -0500

    refactor(users): improve and refine role-based permissions logic

--- core/apps/users/permissions.py
+++ core/apps/users/permissions.py
@@ -2,7 +2,7 @@ from rest_framework.permissions import BasePermission
 from apps.users.constants import Roles
 
 
-class IsAuthenticatedAndHasRole(BasePermission):
+class HasStoreRole(BasePermission):
     allow_roles = []
 
     def has_permission(self, request, view):
@@ -14,40 +14,21 @@ class IsAuthenticatedAndHasRole(BasePermission):
         if user.is_superuser:
             return True
 
+        if not self.allow_roles:
+            return True
+
         return user.tiendas.filter(
             rol__nombre__in=self.allow_roles
         ).exists()
 
 
-class IsOwner(BasePermission):
-    def has_permission(self, request, view):
-        return request.user.is_authenticated and request.user.is_superuser
+class IsOwnerOnly(HasStoreRole):
+    allow_roles = []
 
 
-class IsAdmin(IsAuthenticatedAndHasRole):
+class IsAdminOrOwner(HasStoreRole):
     allow_roles = [Roles.ADMIN]
 
 
-class IsWorker(IsAuthenticatedAndHasRole):
-    allow_roles = [Roles.TRABAJADOR]
-
-
-class CanViewEarnings(IsAuthenticatedAndHasRole):
-    allow_roles = [Roles.ADMIN]
-
-
-class IsAdminOfStore(BasePermission):
-
-    def has_object_permission(self, request, view, obj):
-        user = request.user
-
-        if not user.is_authenticated:
-            return False
-
-        if user.is_superuser:
-            return True
-
-        return user.tiendas.filter(
-            tienda=obj.tienda,
-            rol__nombre=Roles.ADMIN
-        ).exists()
+class IsWorkerOrAdminOrOwner(HasStoreRole):
+    allow_roles = [Roles.TRABAJADOR, Roles.ADMIN]
