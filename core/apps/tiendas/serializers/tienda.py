54ff70e00cf71f8f5cf8f4ba186cb7adb6c309b2
GiomarMC <gmunozcu@unsa.edu.pe>
2026-02-20 11:58:59 -0500

    feat(tiendas): asignar rol DUENO automáticamente al crear tienda
    - Usar transaction.atomic en creación
    - Crear relación UsuarioTienda automáticamente
    - Garantizar consistencia entre usuario y tienda

--- core/apps/tiendas/serializers/tienda.py
+++ core/apps/tiendas/serializers/tienda.py
@@ -1,5 +1,8 @@
 from rest_framework import serializers
 from apps.tiendas.models import Tienda
+from apps.users.models import UsuarioTienda, Rol
+from apps.users.constants import Roles
+from django.db import transaction
 
 
 class TiendaSerializer(serializers.ModelSerializer):
@@ -7,9 +10,23 @@ class TiendaSerializer(serializers.ModelSerializer):
         model = Tienda
         fields = ['id', 'nombre_sede', 'direccion', "created_at"]
 
+    @transaction.atomic
     def create(self, validated_data):
         request = self.context["request"]
-        return Tienda.objects.create(
-            id_duenio=request.user,
+        usuario = request.user
+
+        tienda = Tienda.objects.create(
+            id_duenio=usuario,
             **validated_data
         )
+
+        rol_dueno = Rol.objects.get(nombre=Roles.DUENO)
+
+        UsuarioTienda.objects.create(
+            usuario=usuario,
+            tienda=tienda,
+            rol=rol_dueno,
+            salario=0
+        )
+
+        return tienda
